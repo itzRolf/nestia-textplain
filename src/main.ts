113fea1d109344046d88905eab1b97859c4adc33
itzRolf <109205676+itzRolf@users.noreply.github.com>
2024-03-22 19:14:06 +0200

    add fastify

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const express = await NestFactory.create(AppModule); // text/html when express
+  const fastify = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  ); // text/plain when fastify
+
+  await express.listen(3000);
+  await fastify.listen(3001);
 }
 bootstrap();
